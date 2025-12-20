149a60398bf20b2fd08ce2a784df4f13efb08c02
ValienteRitter <tagizade2006f@gmail.com>
2025-12-20 15:59:07 +0400

    Style: style Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import { NavBar } from "./NavBar";
 
 export function Header() {
     return (
-        <header className=" bg-[#145A32]">
-            <h1>ShelfLife</h1>
+        <header className=" bg-[#145A32] flex flex-row justify-between items-center p-5">
+            <h1 className=" text-[#FDFEFE] text-5xl">ShelfLife</h1>
             <NavBar />
         </header>
     )
