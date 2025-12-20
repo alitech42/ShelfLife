6f15418f51b307834f0ef0512dc8b3a42ba00cb9
ValienteRitter <tagizade2006f@gmail.com>
2025-12-20 15:35:22 +0400

    Feat: Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from "./NavBar";
+
+export function Header() {
+    return (
+        <header className=" bg-[#145A32]">
+            <h1>ShelfLife</h1>
+            <NavBar />
+        </header>
+    )
+}
