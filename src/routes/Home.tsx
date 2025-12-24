c2811295a7741e1975c7e8642cc2d830b4a17293
ValienteRitter <tagizade2006f@gmail.com>
2025-12-24 19:37:32 +0400

    Feat: Create async function for fetching data from openlibrary api

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,17 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export function Home() {
-
-    const [query, setQuery] = useState('')
+    const [query, setQuery] = useState("");
 
     function handleQuery(e: React.ChangeEvent<HTMLInputElement>) {
-        setQuery(e.target.value)
+        setQuery(e.target.value);
     }
 
+    useEffect(() => {
+        async function getBooks(query: string) {
+            const response = await fetch(
+                `https://openlibrary.org/search.json?q=${encodeURIComponent(
+                    query
+                )}`
+            );
+            const data = await response.json();
+
+            console.log(data);
+        }
+        const timeoutID = setTimeout(() => {
+            getBooks(query);
+        }, 1000);
+
+        return () => clearTimeout(timeoutID);
+    }, [query]);
 
     return (
         <main className="flex-6 flex flex-col items-center p-5">
-            <h1 className=" flex justify-center items-center text-7xl w-full flex-1">What have you read today?</h1>
+            <h1 className=" flex justify-center items-center text-7xl w-full flex-1">
+                What have you read today?
+            </h1>
             <div className="flex flex-2 flex-col justify-center items-center w-full">
                 <input
                     type="text"
