1fa08ff2ed375a839f6fe1a0260eee25c45cfae4
ValienteRitter <tagizade2006f@gmail.com>
2025-12-27 20:35:36 +0400

    Feat: add book cover image and "add to list" button

---
+++ src/routes/BookInfo.tsx
@@ -0,0 +1,17 @@
+export function BookInfo() {
+    return (
+        <main className="flex flex-col h-full justify-between items-center p-4">
+            <div className="flex flex-col border rounded-2xl gap-6 p-4">
+                <img
+                    src="https://ia800505.us.archive.org/view_archive.php?archive=/35/items/l_covers_0014/l_covers_0014_65.zip&file=0014658378-L.jpg"
+                    alt="title"
+                />
+                <button className=" border rounded-2xl">Add to List</button>
+                <label>
+                    Your rating: <input type="number" min={0} max={10} />
+                </label>
+            </div>
+
+        </main>
+    );
+}
