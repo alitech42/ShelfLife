eb6e80372a7d83c7783d4cd6e4040b64661eea0b
ValienteRitter <tagizade2006f@gmail.com>
2025-12-27 20:43:37 +0400

    Feat: Add additional information about book
    - Amount of readers who: "Want to read", "Currently reading", "Already read"
    -Description of the Book

--- src/routes/BookInfo.tsx
+++ src/routes/BookInfo.tsx
@@ -1,6 +1,6 @@
 export function BookInfo() {
     return (
-        <main className="flex flex-col h-full justify-between items-center p-4">
+        <main className="flex flex-col h-full justify-between items-center p-4 gap-5">
             <div className="flex flex-col border rounded-2xl gap-6 p-4">
                 <img
                     src="https://ia800505.us.archive.org/view_archive.php?archive=/35/items/l_covers_0014/l_covers_0014_65.zip&file=0014658378-L.jpg"
@@ -11,7 +11,31 @@ export function BookInfo() {
                     Your rating: <input type="number" min={0} max={10} />
                 </label>
             </div>
-
+            <div className="flex flex-row gap-2 border rounded-2xl p-4">
+                <div className="border-r text-center p-3">
+                    <p>Want to read</p>
+                    <p>100</p>
+                </div>
+                <div className="border-r text-center p-3">
+                    <p>Currently reading</p>
+                    <p>100</p>
+                </div>
+                <div className=" text-center p-3">
+                    <p>Already read</p>
+                    <p>100</p>
+                </div>
+            </div>
+            <div>
+                <p>
+                    For a thousand years ashes have fallen from the sky. For a
+                    thousand years nothing has flourished. For a thousand years
+                    the skaa have been enslaved and live in misery, mired in
+                    inevitable fear. For a thousand years the Lord Ruler has
+                    reigned with absolute power, ruling thanks to terror, his
+                    powers and his immortality, aided by "benders" and
+                    "inquisitors", along with the powerful magic of Allomancy.
+                </p>
+            </div>
         </main>
     );
 }
