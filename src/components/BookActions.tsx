5cbd7b61dc0a8edc6f21ab6097b673f716b70885
ValienteRitter <tagizade2006f@gmail.com>
2026-01-03 11:50:12 +0400

    Chore: Format with Prettier

--- src/components/BookActions.tsx
+++ src/components/BookActions.tsx
@@ -6,7 +6,7 @@ type BookActionsProps = {
 export function BookActions({ title, cover }: BookActionsProps) {
     return (
         <div className="flex flex-col border rounded-2xl gap-6 p-4 sm:row-end-2 sm:col-end-1">
-            { cover !== "" ? <img src={cover} alt={title} /> : <h1>{title}</h1>}
+            {cover !== "" ? <img src={cover} alt={title} /> : <h1>{title}</h1>}
             <button className=" border rounded-2xl">Add to List</button>
             <label>
                 Your rating: <input type="number" min={0} max={10} />
