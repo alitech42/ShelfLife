c6cad743a2d454fc43ce72e0e3fd5f57da61a71c
ValienteRitter <tagizade2006f@gmail.com>
2026-02-12 22:18:52 +0400

    Feat: Link to home page when title clicked

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import { NavBar } from "./NavBar";
+import { Link } from "react-router";
 
 export function Header() {
     return (
         <header className=" bg-[#145A32] flex flex-row justify-between items-center p-5 flex-1">
-            <h1 className=" text-[#FDFEFE] text-5xl">ShelfLife</h1>
+            <Link to={"/home"}><h1 className=" text-[#FDFEFE] text-5xl">ShelfLife</h1></Link>
             <NavBar />
         </header>
     );
