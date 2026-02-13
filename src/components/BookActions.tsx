8fd4f2f0cc0ae6e667a8caff31d9f8ac3338c17b
ValienteRitter <tagizade2006f@gmail.com>
2026-02-13 20:08:00 +0400

    Feat: Save book to LocalStorage when clicked on AddToList button

--- src/components/BookActions.tsx
+++ src/components/BookActions.tsx
@@ -1,13 +1,22 @@
+import { useState, useEffect } from "react";
+import { saveToLocalStorage } from "../utilities";
+
 type BookActionsProps = {
     title: string;
     cover: string;
 };
 
 export function BookActions({ title, cover }: BookActionsProps) {
+    function handleClick() {
+        saveToLocalStorage(title);
+    }
+
     return (
         <div className="flex flex-col border rounded-2xl gap-6 p-4 sm:shrink-0">
             {cover !== "" ? <img src={cover} alt={title} /> : <h1>{title}</h1>}
-            <button className=" border rounded-2xl">Add to List</button>
+            <button className=" border rounded-2xl" onClick={handleClick}>
+                Add to List
+            </button>
             <label>
                 Your rating: <input type="number" min={0} max={10} />
             </label>
