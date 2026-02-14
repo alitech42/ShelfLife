45392cdc6253af235e7c966b369efa75e2a81a64
ValienteRitter <tagizade2006f@gmail.com>
2026-02-14 23:19:35 +0400

    Style: Highlight add to list button when hovered

--- src/components/BookActions.tsx
+++ src/components/BookActions.tsx
@@ -14,7 +14,7 @@ export function BookActions({ title, cover, olid }: BookActionsProps) {
     return (
         <div className="flex flex-col border rounded-2xl gap-6 p-4 sm:shrink-0">
             {cover !== "" ? <img src={cover} alt={title} /> : <h1>{title}</h1>}
-            <button className=" border rounded-2xl" onClick={handleClick}>
+            <button className=" border rounded-2xl hover:bg-green-900 hover:text-white hover:scale-105 transition-all" onClick={handleClick}>
                 Add to List
             </button>
             <label>
