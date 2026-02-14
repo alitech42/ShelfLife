36d34757f88bf8ff6da6fca6c93f0233a62b821a
ValienteRitter <tagizade2006f@gmail.com>
2026-02-14 22:15:40 +0400

    Style: Highlight the hovered query result.

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -9,7 +9,7 @@ type BookCardProps = {
 export function BookCard({ title, author, bookRoute }: BookCardProps) {
     return (
         <Link to={bookRoute}>
-            <div className=" p-3 border rounded-2xl ">
+            <div className=" p-3 border rounded-2xl hover:bg-green-900 hover:text-white transition-colors">
                 <p className=" font-bold">{title}</p>
                 {author && <p className=" font-extralight">by {author}</p>}
             </div>
