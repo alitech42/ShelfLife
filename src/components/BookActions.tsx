866d984d2621f5b9502f60477a68b39494cd2155
ValienteRitter <tagizade2006f@gmail.com>
2026-02-15 22:44:43 +0400

    Feat: add localStorage state sync and conditional "On the List" button

--- src/components/BookActions.tsx
+++ src/components/BookActions.tsx
@@ -1,4 +1,6 @@
 import { saveToLocalStorage } from "../utilities";
+import type { listData } from "../types";
+import { useState, useEffect } from "react";
 
 type BookActionsProps = {
     title: string;
@@ -7,19 +9,35 @@ type BookActionsProps = {
 };
 
 export function BookActions({ title, cover, olid }: BookActionsProps) {
+    const [list, setList] = useState<listData[]>(() => {
+        const stored = localStorage.getItem("data");
+        return stored ? JSON.parse(stored) : [];
+    });
+
+    const isAdded = list.some((book: listData) => book.olid === olid);
+    useEffect(() => console.log(list), []);
+
     function handleClick() {
         saveToLocalStorage(title, cover, olid);
+        setList(JSON.parse(localStorage.getItem("data") ?? "[]"));
     }
 
     return (
         <div className="flex flex-col border rounded-2xl gap-6 p-4 sm:shrink-0">
             {cover !== "" ? <img src={cover} alt={title} /> : <h1>{title}</h1>}
-            <button
-                className=" border rounded-2xl hover:bg-green-900 hover:text-white hover:scale-105 transition-all"
-                onClick={handleClick}
-            >
-                Add to List
-            </button>
+            {isAdded ? (
+                <button className=" border rounded-2xl bg-green-900 text-white scale-105">
+                    On the List
+                </button>
+            ) : (
+                <button
+                    className=" border rounded-2xl hover:bg-green-900 hover:text-white hover:scale-105 transition-all"
+                    onClick={handleClick}
+                >
+                    Add to List
+                </button>
+            )}
+
             <label>
                 Your rating: <input type="number" min={0} max={10} />
             </label>
