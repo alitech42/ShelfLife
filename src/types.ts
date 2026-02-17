c723bab465bcf3d9565ef33ea7141eaacc4fb050
ValienteRitter <tagizade2006f@gmail.com>
2026-02-17 18:59:47 +0400

    Feat: Create function for updating rating values.

--- src/types.ts
+++ src/types.ts
@@ -34,4 +34,5 @@ export type listData = {
     title: string;
     cover?: string;
     olid: string;
+    rating: string
 };
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,10 +1,21 @@
 import type { listData } from "./types";
 
-export function saveToLocalStorage(title: string, cover: string, olid: string) {
+export function saveToLocalStorage(title: string, cover: string, olid: string, rating: string) {
     const existing = localStorage.getItem("data");
     const arr: listData[] = existing ? JSON.parse(existing) : [];
 
-    const updatedArr = [...arr, { title, cover, olid }];
+    const updatedArr = [...arr, { title, cover, olid, rating }];
+
+    localStorage.setItem("data", JSON.stringify(updatedArr));
+}
+
+export function updateLocalStorage(olid: string, userRating: string) {
+    const existing = localStorage.getItem("data");
+    const arr: listData[] = existing ? JSON.parse(existing) : [];
+
+    const updatedArr = arr.map(book => {
+        return book.olid === olid ? {...book, rating: userRating} : book
+    })
 
     localStorage.setItem("data", JSON.stringify(updatedArr));
 }
